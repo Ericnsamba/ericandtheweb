96043873c7be1cdad21f660c893c7581c589bd45
Eric&TheWeb <43747486+Ericnsamba@users.noreply.github.com>
2022-03-20 23:42:52 +0100

    Revert "fixed type error"
    This reverts commit c94670a941b43401c847190528afe238c993a657.

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,8 +11,11 @@ const NavBar = ({ fixed }) => {
   return (
     <div className="flex flex-wrap py-2 mt-3">
       <div className="w-full px-4">
+      <div className="lg:hidden flex justify-end mb-2">
+                  <ToggleButton />
+                </div>
         <nav className="relative flex flex-wrap items-center justify-between px-2 py-3  rounded-xl border border-slate-200 ">
-          <div className="container mx-auto flex  items-center justify-between">
+          <div className="container mx-auto flex flex-wrap items-center justify-between">
             <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
               <div className="flex items-cente">
                 <Link href="/">
@@ -28,13 +31,13 @@ const NavBar = ({ fixed }) => {
                   <strong className="text-forestGreen dark:text-green font-display font-body">
                     Eric & The Web
                   </strong>
-                  <span className="text-slate-300	font-body font-light dark:text-green opacity-50">
+                  <span className="text-slate-400	font-body font-light">
                     Designer & Developer
                   </span>
                 </div>
               </div>
 
-              {/* <button
+              <button
                 className="text-green cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
                 type="button"
                 onClick={() => setMenuOpen(!menuOpen)}
@@ -43,20 +46,63 @@ const NavBar = ({ fixed }) => {
                   {menuOpen === true ? (
                     <X
                       size={24}
-                      className="text-black cursor-pointer hover:text-green motion-reduce:animate-bounce	"
+                      className="text-forestGreen dark:text-green cursor-pointer hover:text-green motion-reduce:animate-bounce	"
                     />
                   ) : (
                     <Menu
                       size={24}
-                      className="text-black cursor-pointer hover:text-green "
+                      className="text-forestGreen dark:text-green cursor-pointer hover:text-green "
                     />
                   )}
                 </div>
-              </button> */}
+              </button>
             </div>
-              <div className="justify-end">
-                <ToggleButton/>
+            <div
+              className={
+                "lg:flex flex-grow items-center justify-center" +
+                (menuOpen ? " flex" : " hidden")
+              }
+              id="example-navbar-info"
+            >
+              <div className="flex flex-col items-center lg:flex-row list-none lg:ml-auto lg:w-1/2	px-8 justify-between">
+                <Link href="/">
+                  <a
+                    onClick={() => setMenuOpen(false)}
+                    className="menuItems text-center py-4 font-body uppercase text-sm	text-forestGreen dark:text-green opacity-50	hover:opacity-100"
+                  >
+                    Home
+                  </a>
+                </Link>
+                <Link href="/about">
+                  <a
+                    onClick={() => setMenuOpen(false)}
+                    className="menuItems text-center py-4 font-body uppercase text-sm	text-forestGreen dark:text-green opacity-50	hover:opacity-100"
+                  >
+                    About me
+                  </a>
+                </Link>
+                {/* <Link href="/portfolio">
+                  <a
+                    onClick={() => setMenuOpen(false)}
+                    className="menuItems text-center py-4 font-body uppercase text-sm	text-forestGreen dark:text-green opacity-50	hover:opacity-100"
+                  >
+                    Portfolio
+                  </a>
+                </Link> */}
+                <Link href="/contact">
+                  <a
+                    onClick={() => setMenuOpen(false)}
+                    className="menuItems text-center py-4 font-body uppercase text-sm	text-forestGreen dark:text-green opacity-50	hover:opacity-100"
+                  >
+                    Contact
+                  </a>
+                </Link>
+                <div className="lg:flex hidden">
+                  <ToggleButton />
+                </div>
+                {/* <ToggleButton className="menuItems"/> */}
               </div>
+            </div>
           </div>
         </nav>
       </div>
