7dc568e27dd71c222d1e6b83757df377c268ab5a
Eric&TheWeb <43747486+Ericnsamba@users.noreply.github.com>
2024-09-25 10:19:22 +0100

    config updates

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  optimizeFonts: true,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-		domains: ['cdn.sanity.io'],
-		loader: 'default',
-    // loaderFile: '',
-	},
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+//   optimizeFonts: true,
+//   experimental: {
+//     appDir: true,
+//   },
+//   images: {
+// 		domains: ['cdn.sanity.io'],
+// 		loader: 'default',
+//     // loaderFile: '',
+// 	},
+// }
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
 }
+ 
+module.exports = nextConfig
