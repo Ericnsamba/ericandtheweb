86bcae5d46358b765a4d7b73bf1dfef456b93ed5
Eric&TheWeb <43747486+Ericnsamba@users.noreply.github.com>
2024-10-06 22:14:08 +0100

    clean up

--- components/PageNav.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const PageNav = ({ pageNavStyles, navName }: { pageNavStyles: string, navName: string }) => (
-  <div className={`${pageNavStyles} text-lg text-black h-full font-display`}>{navName}</div>
-)
-
-export default PageNav
--- sanity/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import portfolio from './portfolio'
-import workExperience from './workExperience'
-
-export const schemaTypes = [portfolio, post, category, workExperience]
