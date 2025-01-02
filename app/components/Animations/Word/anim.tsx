d567471ba261e01ebafd3048423459c3bc32e700
Eric&TheWeb <43747486+Ericnsamba@users.noreply.github.com>
2025-01-02 00:23:02 +0000

    Update slideUp animation duration and delay in anim.tsx for smoother transitions

--- app/components/Animations/Word/anim.tsx
+++ app/components/Animations/Word/anim.tsx
@@ -4,7 +4,7 @@ export const slideUp = {
     },
     open: (i: number) => ({
         y: "0%",
-        transition: {duration: 0.8, delay: 0.01 * i}
+        transition: {duration: 1, delay: 0.04 * i}
     }),
     closed: {
         y: "100%",
