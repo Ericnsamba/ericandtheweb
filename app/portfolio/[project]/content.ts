d0cf1cf1fd842554644ddae102b9b2b2b0705300
Eric&TheWeb <43747486+Ericnsamba@users.noreply.github.com>
2025-09-03 20:11:26 +0100

    clean up

--- app/portfolio/[project]/content.ts
+++
@@ -1,2 +0,0 @@
-export const restrictedTitle = "This project is still under wraps for now.";
-export const restrictedMessage = "This project is currently a work in progress and remains under wraps for now. Stay tuned for updates and a closer look once it's ready to be revealed!";
